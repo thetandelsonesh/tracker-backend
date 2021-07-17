1d96f341f9ffaf2136e3ed2a5e9b716c48aa623c
Sonesh Tandel <thetandelsonesh@gmail.com>
2021-07-18 01:18:38 +0530

    handle epoch timestamp

--- api/place/placeInteraction.js
+++ api/place/placeInteraction.js
@@ -1,14 +1,31 @@
 const { Op, fn, col } = require('sequelize');
+const Joi = require('joi');
+
 const { vehicle, track, place } = require('../../db/models');
-const { ERROR_500, INVALID_DATA } = require('../../constants/errorCodes')
+const { ERROR_500, INVALID_DATA, NO_DATA_FOUND } = require('../../constants/errorCodes')
 
 const limit = 50;
 
-const placeInteraction = async (req, res) => {
-  try{
-    const {placeId, startDate, endDate, page = 1} = req.query;
+const schema = Joi.object({
+  placeId: Joi.number().integer().min(1).required(),
+  startDate: Joi.date().timestamp('unix').required(),
+  endDate: Joi.date().timestamp('unix').required(),
+  page: Joi.number().integer().min(1).required(),
+});
 
-    const offset = ((page - 1) * limit);
+const placeInteraction = async (req, res) => {
+  const {placeId, startDate, endDate, page = 1} = req.query;
+
+  try {
+    await schema.validateAsync({placeId, startDate, endDate, page});
+  } catch (e) {
+    res.send(INVALID_DATA);
+    return;
+  }
+
+  const offset = ((page - 1) * limit);
+
+  try{
 
     const placeData = await place.findOne({
       where: {
@@ -21,25 +38,22 @@ const placeInteraction = async (req, res) => {
       return;
     }
 
-    const contains = fn('ST_CONTAINS',
-      placeData.border,
-      col('location')
-    );
-    console.log(contains);
-
-    const data = await track.findAll({
-      where: contains,
-      // where: {
-      //   location: []
-      //   trackedAt: {
-      //     [Op.between]: [new Date(startDate), new Date(endDate).setHours(23,59,59,999)]
-      //   }
-      // },
-      // include: [{model: vehicle}],
-      // limit: limit,
-      // offset: offset,
-      // order: [['trackedAt', 'ASC']]
+    const data = await track.findAndCountAll({
+      where: {
+        trackedAt: {
+          [Op.between]: [new Date(startDate*1000), new Date(endDate*1000)]
+        }
+      },
+      include: [{model: vehicle}],
+      limit: limit,
+      offset: offset,
+      order: [['trackedAt', 'ASC']]
     });
+
+    if(data.count === 0){
+      res.send(NO_DATA_FOUND);
+      return;
+    }
 
     res.send({
       payload: {
@@ -49,7 +63,7 @@ const placeInteraction = async (req, res) => {
       msg: "Place Interaction List"
     });
   }catch (e){
-    res.send(e);
+    res.send(ERROR_500);
   }
 }
 
--- api/vehicle/vehicleActivity.js
+++ api/vehicle/vehicleActivity.js
@@ -1,16 +1,31 @@
 const { Op } = require('sequelize');
+const Joi = require('joi');
+
 const { track } = require('../../db/models');
-const { ERROR_500, NO_DATA_FOUND } = require('../../constants/errorCodes')
+const { ERROR_500, NO_DATA_FOUND, INVALID_DATA } = require('../../constants/errorCodes')
+
+const schema = Joi.object({
+  vehicleId: Joi.number().integer().min(1).required(),
+  startDate: Joi.date().timestamp('unix').required(),
+  endDate: Joi.date().timestamp('unix').required(),
+});
 
 const vehicleActivity = async (req, res) => {
-  try{
-    const {startDate, endDate, vehicleId} = req.query;
+  const {startDate, endDate, vehicleId} = req.query;
 
+  try {
+    await schema.validateAsync({startDate, endDate, vehicleId});
+  } catch (e) {
+    res.send(INVALID_DATA);
+    return;
+  }
+
+  try{
     const list = await track.findAll({
       where: {
         vehicleId: vehicleId,
         trackedAt: {
-          [Op.between]: [new Date(startDate), new Date(endDate).setHours(23,59,59,999)]
+          [Op.between]: [new Date(startDate*1000), new Date(endDate*1000)]
         }
       },
       order: [['trackedAt', 'ASC']]
